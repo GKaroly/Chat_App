4b9f8f3db7989b7dfddc04ed18b163a7cf0cddf5
GKaroly <gabanyikarolyarpad@gmail.com>
2017-06-18 02:34:24 +0200

    other session login tryed

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 #!/usr/bin/python3
 
 import flask #install version: 0.12.2
+import os
 
 from flask import Flask 
 from flask import render_template
 from functools import wraps
-from flask import request, Response
+from flask import request, Response, session, flash
 
 app = Flask(__name__)
 
+#https://pythonspot.com/en/login-authentication-with-flask/
+@app.route('/')
+def home():
+    if not session.get('logged_in'):
+        return render_template('mainsite.html')
+    else:
+        return "Hello Boss!"
+ 
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return home()
+
+"""
 def check_auth(username, password):
-    """This function is called to check if a username /
-    password combination is valid.
-    """
+    """"""This function is called to check if a username /
+    password combination is valid.""""""
     return username == 'admin' and password == 'secret'
 
 @app.route("/")
@@ -39,8 +56,9 @@ def sentMessage():
 	print(username)
 	print(message)
 	return "dfsfdsfsd"#render_template("appsite.html")
-
+"""
 
 if __name__ == "__main__":
+	app.secret_key = os.urandom(12)
 	print("MyProgram started running")
 	app.run(host="127.0.0.1", port=4000)
