20f8e2e8164fa3da9c9b361ae259bc4c145327f4
GKaroly <gabanyikarolyarpad@gmail.com>
2017-06-18 02:11:43 +0200

    site after login
    It has many problems, multiple user handling must added, message to flask and return value must handle

--- main.py
+++ main.py
@@ -4,8 +4,16 @@ import flask #install version: 0.12.2
 
 from flask import Flask 
 from flask import render_template
+from functools import wraps
+from flask import request, Response
 
 app = Flask(__name__)
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'admin' and password == 'secret'
 
 @app.route("/")
 def hello():
@@ -14,8 +22,24 @@ def hello():
 
 @app.route("/login", methods=['POST'])
 def login():
-	return "Szia2"
+	username=request.form['username']
+	password=request.form['password']
+	if check_auth(username, password):
+		return render_template("appsite.html")
+	else:
+		return redirect('/')
 #http://flask.pocoo.org/docs/0.12/tutorial/
+
+
+@app.route("/send", methods=['POST'])
+def sentMessage():
+	username=request.form['username']
+	message=request.form['message']
+
+	print(username)
+	print(message)
+	return "dfsfdsfsd"#render_template("appsite.html")
+
 
 if __name__ == "__main__":
 	print("MyProgram started running")
